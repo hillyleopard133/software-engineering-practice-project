058c5473a8f085bdbbd5d70f80257c9317b4c8c1
Evan Hearne <evanhearne02@gmail.com>
2023-04-16 11:57:20 +0100

    Added methods getGrade and getModule

--- src/Student.java
+++ src/Student.java
@@ -5,4 +5,22 @@ public class Student {
     private int id;
     private String course;
     private Module[] modules;
+    public Module getModule(String moduleName) {
+        for (Module module: modules){
+            if (module.getName().equals(moduleName))
+                return module;
+        }
+        return null;
+    }
+    public int getGrade(String moduleName, String assignment) {
+        int count = 0;
+        if (getModule(moduleName) != null)
+            for (String assignment1: getModule(moduleName).getAssignments()) {
+                count++;
+                if (assignment.equals(assignment1))
+                    return getModule(moduleName).getGrades()[count];
+            }
+        return -1;
+    }
+
 }
