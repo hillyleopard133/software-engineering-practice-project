9dd6e5b51bb9f8727a4310930852deeffa849844
Evan Hearne <evanhearne02@gmail.com>
2023-04-16 11:45:17 +0100

    Added fields

--- src/Module.java
+++ src/Module.java
@@ -1,2 +1,12 @@
+import java.time.LocalDate;
+
 public class Module {
+    // Fields.
+    private String name;
+    private String[] lecturers;
+    private String[] assignments;
+    private int[] grades;
+    private int code;
+    private Student student;
+    private LocalDate dateCompleted;
 }
