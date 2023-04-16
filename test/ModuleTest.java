07e66191a5c8efbdf772ef2a95c12f8ddf8aea48
Evan Hearne <evanhearne02@gmail.com>
2023-04-16 21:01:52 +0100

    Created test classes for Module.java and Student.java in src, for Grades class

---
+++ test/ModuleTest.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+class ModuleTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getLecturers() {
+    }
+
+    @Test
+    void setLecturers() {
+    }
+
+    @Test
+    void getAssignments() {
+    }
+
+    @Test
+    void setAssignments() {
+    }
+
+    @Test
+    void getGrades() {
+    }
+
+    @Test
+    void setGrades() {
+    }
+
+    @Test
+    void getCode() {
+    }
+
+    @Test
+    void setCode() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void setStudent() {
+    }
+
+    @Test
+    void getDateCompleted() {
+    }
+
+    @Test
+    void setDateCompleted() {
+    }
+
+    @Test
+    void listAllLecturers() {
+    }
+
+    @Test
+    void listAllGrades() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
---
+++ test/StudentTest.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getModule() {
+    }
+
+    @Test
+    void getGrade() {
+    }
+
+    @Test
+    void getAllGrades() {
+    }
+
+    @Test
+    void sortGradesLowest() {
+    }
+
+    @Test
+    void sortGradesHighest() {
+    }
+}
