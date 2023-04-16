5fb439c8b7e34f14e68659e34ca172ff9fdcfa65
Evan Hearne <evanhearne02@gmail.com>
2023-04-16 11:58:26 +0100

    Added getters and setters.

--- src/Module.java
+++ src/Module.java
@@ -1,6 +1,7 @@
 import java.time.LocalDate;
 
 public class Module {
+
     // Fields.
     private String name;
     private String[] lecturers;
@@ -9,4 +10,61 @@ public class Module {
     private int code;
     private Student student;
     private LocalDate dateCompleted;
+
+    // Getters and Setters.
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String[] getLecturers() {
+        return lecturers;
+    }
+
+    public void setLecturers(String[] lecturers) {
+        this.lecturers = lecturers;
+    }
+
+    public String[] getAssignments() {
+        return assignments;
+    }
+
+    public void setAssignments(String[] assignments) {
+        this.assignments = assignments;
+    }
+
+    public int[] getGrades() {
+        return grades;
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public LocalDate getDateCompleted() {
+        return dateCompleted;
+    }
+
+    public void setDateCompleted(LocalDate dateCompleted) {
+        this.dateCompleted = dateCompleted;
+    }
 }
