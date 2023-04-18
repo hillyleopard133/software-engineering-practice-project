a2ee717bff3e96c6d46751a14618471f6a07fb14
hillyleopard133 <91201521+hillyleopard133@users.noreply.github.com>
2023-04-18 12:47:07 +0100

    Update Student.java

--- SoftwareEngineering/src/Student.java
+++ SoftwareEngineering/src/Student.java
@@ -8,6 +8,7 @@ public class Student {
     private int studentID = 0;
     private String course = "";
     private ArrayList<Club> clubsJoined = new ArrayList<>();
+    private Module[] modules;
 
     // Constructor
 
@@ -37,6 +38,51 @@ public class Student {
         return list;
     }
 
+    public Module getModule(String moduleName) {
+        for (Module module: modules){
+            if (module.getName().equals(moduleName))
+                return module;
+        }
+        return null;
+    }
+    
+    public String sortGradesLowest() {
+        ArrayList<Module> moduleList = new ArrayList<>();
+        Collections.addAll(moduleList, modules);
+        for (Module module: modules) {
+            moduleList.add(module);
+        }
+        for (Module moduleL : moduleList) {
+            ArrayList<Integer> grades = new ArrayList<>();
+            for (int grade : moduleL.getGrades()) {
+                grades.add(grade);
+            }
+            Collections.sort(grades);
+            moduleL.setGrades(grades.stream().mapToInt(i->i).toArray());
+        }
+        modules = moduleList.toArray(new Module[moduleList.size()]);
+        return getAllGrades();
+    }
+    
+    public String sortGradesHighest() {
+        ArrayList<Module> moduleList = new ArrayList<>();
+        Collections.addAll(moduleList, modules);
+        for (Module module: modules) {
+            moduleList.add(module);
+        }
+        for (Module moduleL : moduleList) {
+            ArrayList<Integer> grades = new ArrayList<>();
+            for (int grade : moduleL.getGrades()) {
+                grades.add(grade);
+            }
+            Collections.sort(grades);
+            Collections.reverse(grades);
+            moduleL.setGrades(grades.stream().mapToInt(i->i).toArray());
+        }
+        modules = moduleList.toArray(new Module[moduleList.size()]);
+        return getAllGrades();
+    }
+    
     // Getters and Setters
 
     public String getName() {
@@ -70,5 +116,33 @@ public class Student {
     public void setClubsJoined(ArrayList<Club> clubsJoined) {
         this.clubsJoined = clubsJoined;
     }
+    
+    public Module[] getModules() {
+        return modules;
+    }
+
+    public void setModules(Module[] modules) {
+        this.modules = modules;
+    }
+    
+    public int getGrade(String moduleName, String assignment) {
+        int count = 0;
+        if (getModule(moduleName) != null)
+            for (String assignment1: getModule(moduleName).getAssignments()) {
+                if (assignment.equals(assignment1))
+                    return getModule(moduleName).getGrades()[count];
+                count++;
+            }
+        return -1;
+    }
+    
+    public String getAllGrades(){
+        String allGrades = "";
+        for (Module module: modules) {
+            allGrades += module.toString();
+        }
+        return allGrades;
+    }
+
 }
 
